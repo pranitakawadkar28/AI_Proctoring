55c04c3286b6f35c9400a93370df8c6ea7655cdd
Pranita Kawadkar <pranitakawadkar10@gmail.com>
2026-02-05 20:47:37 +0530

    feat: Protects routes based on user roles (admin, teacher, student)

---
+++ server/src/routes/app.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { protect } from "../middlewares/auth.middleware.js";
+import { restrictTo } from "../middlewares/rbac.middleware.js";
+
+const router = express.Router();
+
+// Admin can create teachers/admins
+router.post("/users", protect, restrictTo("admin"), );
+
+// Teacher can create tests
+router.post("/tests", protect, restrictTo("teacher"), );
+
+// Student can attempt tests
+router.post("/tests/:id/attempt", protect, restrictTo("student"), );
+
+export default router
